f35d838705054c5f9c8aed059b420d9f06518663
boabab <89863585+boabab@users.noreply.github.com>
2023-04-11 18:02:53 +0000

    fix: bug in RomanNumerals

--- javueb2/RomanNumerals.java
+++ javueb2/RomanNumerals.java
@@ -39,13 +39,13 @@ public class RomanNumerals {
 				output += "D";
 				number -= 500;
 			}
-			else if (number > 400) {
+			else if (number >= 400) {
 				output += "CD";
 				number -= 400;
 			}
 			
 			// 100er
-			else if (number < 400 && number >= 100) {
+			else if (number >= 100) {
 				output += "C";
 				number -= 100;
 			}
@@ -57,17 +57,17 @@ public class RomanNumerals {
 				output += "L";
 				number -= 50;
 			}
-			else if (number > 40) {
+			else if (number >= 40) {
 				output += "XL";
 				number -= 40;
 			}
 
 			// 10er
-			else if (number < 40 && number >= 10) {
+			else if (number >= 10) {
 				output += "X";
 				number -= 10;
 			}
-			else if (number == 9) {
+			else if (number >= 9) {
 				output += "IX";
 				number -= 9;
 			}
@@ -75,11 +75,11 @@ public class RomanNumerals {
 				output += "V";
 				number -= 5;
 			}
-			else if (number == 4) {
+			else if (number >= 4) {
 				output += "IV";
 				number -= 4;
 			}
-			else if (number < 4) {
+			else if (number >= 1) {
 				output += "I";
 				number -= 1;
 			}
